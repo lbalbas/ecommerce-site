4874e885dcb17303dd9919662681090e46ecaeed
Luis Balbás <lbq200@gmail.com>
2023-04-19 17:14:37 -0400

    Styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import type { ChildrenProps } from "../utils/globalTypes";
 
 const PrimaryButton = (props: Props) => {
 	return (
-		<button className="w-20 text-white font-bold py-1.5  flex items-center justify-center rounded-2xl bg-gradient-to-r from-violet-500 to-fuchsia-500">
+		<button className="w-20 text-ivory font-bold py-1.5  flex items-center justify-center rounded-2xl bg-trueblue">
 			{props.children}
 		</button>
 	);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import Cart from "./Cart";
 
 const Header = () => {
 	return (
-		<header className="flex justify-around items-center h-16 bg-cyan-100">
-			<Link href="/">Logo</Link>
+		<header className="flex bg-raisin justify-around items-center h-16">
+			<Link className="text-ivory" href="/">Logo</Link>
 			<input
 				type="text"
 				className="min-w-64 w-full md:max-w-[30vw] self-center rounded-2xl p-1"
