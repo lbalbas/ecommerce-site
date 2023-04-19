6854a7f0870434e1c03fd9b54963d68df6517827
Luis Balbás <lbq200@gmail.com>
2023-04-19 17:14:02 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,22 +7,24 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<CartProvider>
-			<ToastContainer
-				position="top-right"
-				autoClose={4000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme="light"
-			/>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</CartProvider>
+		<div className="bg-ivory text-raisin min-h-screen">
+			<CartProvider>
+				<ToastContainer
+					position="top-right"
+					autoClose={4000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="light"
+				/>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</CartProvider>
+		</div>
 	);
 }
