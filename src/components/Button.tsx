d50a54ba110eed8a6bebd2c127b5e278d175100e
Luis Balbás <lbq200@gmail.com>
2023-04-29 14:58:48 -0400

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { Props } from "../utils/globalTypes";
 
 const PrimaryButton = (props: Props) => {
 	return (
-		<button className="w-20 text-ivory font-bold py-1.5  flex items-center justify-center rounded-full bg-trueblue">
+		<button className="min-w-fit max-h-10 px-4 text-ivory font-bold py-1.5  flex items-center justify-around rounded-full bg-trueblue">
 			{props.children}
 		</button>
 	);
