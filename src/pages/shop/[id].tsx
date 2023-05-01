fcbdb1ef463e1246d43e0b7727a9b6c9baaaae96
Luis Balbás <lbq200@gmail.com>
2023-05-01 18:17:57 -0400

    Update [id].tsx

--- src/pages/shop/[id].tsx
+++ src/pages/shop/[id].tsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
 import { useCart } from "@/context/CartContext";
 import Image from "next/image";
-import Button from '../../components/Button'
-import Counter from '../../components/ItemCounter'
+import Button from "../../components/Button";
+import Counter from "../../components/ItemCounter";
 import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import FeaturedItems from "../../components/FeaturedItems";
 
 const ProductPage = () => {
 	const { addItemToCart } = useCart();
@@ -17,25 +18,37 @@ const ProductPage = () => {
 		img: "/placeimg_720_720_any.jpeg",
 	};
 	return (
-		<div className="grid grid-cols-2 lg:h-[60vh]">
-			<div className="relative w-full">
-				<Image alt="Product" src="/placeimg_720_720_any.jpeg" fill />
-			</div>
-			<div className="flex w-full justify-center items-center">
-				<div className="lg:w-5/6 flex-col flex">
-					<div className="flex items-baseline">
-					<h1 className="text-2xl text-raisin font-bold">
-						{placeholder.name}
-					</h1>
-					<h3 className="mx-4 text-gray-600">${placeholder.price}</h3>
-					</div>
-					<p className="my-4 text-gray-700">{placeholder.desc}</p>
-					<div className="flex items-center w-full justify-end gap-8">
-					 <Counter />
-					 <Button><ShoppingCartIcon className="h-6 w-6" />Add to Cart</Button>
+		<div className="flex flex-col">
+			<div className="grid grid-cols-2 lg:h-[65vh] mb-6">
+				<div className="relative w-full">
+					<Image
+						alt="Product"
+						src="/placeimg_720_720_any.jpeg"
+						fill
+					/>
+				</div>
+				<div className="flex w-full justify-center items-center">
+					<div className="lg:w-5/6 flex-col flex">
+						<div className="flex items-baseline">
+							<h1 className="text-2xl text-raisin font-bold">
+								{placeholder.name}
+							</h1>
+							<h3 className="mx-4 text-gray-600">
+								${placeholder.price}
+							</h3>
+						</div>
+						<p className="my-4 text-gray-700">{placeholder.desc}</p>
+						<div className="flex items-center w-full justify-end gap-8">
+							<Counter />
+							<Button>
+								<ShoppingCartIcon className="h-6 w-6" />
+								Add to Cart
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
+			<FeaturedItems />
 		</div>
 	);
 };
