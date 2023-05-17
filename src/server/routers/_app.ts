bfeb44599118ffb6c0a1314436aab6e2b5eddfa4
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-05-17 15:16:02 -0400

    fetch item

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,29 +1,28 @@
 import { procedure, router } from "../trpc";
 import { PrismaClient } from "@prisma/client";
-
+import { z } from "zod";
 const prisma = new PrismaClient();
 
-async function main() {
-  const allItems = await prisma.items.findMany();
-  return convertBigIntsToStrings(allItems);
-}
-
-function convertBigIntsToStrings(items) {
-  return items.map((item) => ({
-    ...item,
-    id: item.id.toString(),
-    collection: item.collection ? item.collection.toString() : null,
-    stock: item.stock.toString(),
-  }));
-}
-
 export const appRouter = router({
+  item: procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async (opts) => {
+      const item = await prisma.items.findUnique({
+        where: { id: opts.input.id },
+      }); // Wait for the result of main() function
+      await prisma.$disconnect(); // Disconnect from Prisma after getting the result
+      return item;
+    }),
   items: procedure.query(async () => {
-    const items = await main(); // Wait for the result of main() function
+    const items = await prisma.items.findMany(); // Wait for the result of main() function
     await prisma.$disconnect(); // Disconnect from Prisma after getting the result
     return items;
   }),
-  featured: procedure.query(async () => {
+  featured: procedure.query(async (id) => {
     const featured = await prisma.items.findMany({ take: 4 });
     await prisma.$disconnect(); // Disconnect from Prisma after getting the result
     return convertBigIntsToStrings(featured);
