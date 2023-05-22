338351c782f8624c207dd5c07d6b8b992b392488
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-05-22 15:03:42 -0400

    Add collections

---
+++ src/pages/collections/index.tsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from 'react'
+import { trpc } from "../../utils/trpc";
+import Head from 'next/head'
+
+const Collections = () => {
+	const [collections, setCollections] = useState([])
+	const collectionsData = trpc.collections.useQuery();
+
+	useEffect(() => {
+	  if (collectionsData.data) {
+	    setCollections(collectionsData.data);
+	  }
+	}, [collectionsData.data]);
+
+	if(collectionsData.isLoading)
+		return (
+			<div>
+				<Head>
+	  	 			<title>Collections</title>
+	  			</Head>
+				Loading...
+			</div>)
+
+	return (
+	  <div>
+	  	<Head>
+	  	 <title>Collections</title>
+	  	</Head>
+	  	{collections.map((coll, key) => {return coll})}
+	  </div>
+	)
+}
+
+export default Collections;
