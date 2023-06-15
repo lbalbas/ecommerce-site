abd23c7041da915cc31c45d3d563f5f87fbb33c4
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-06-15 17:26:00 -0400

    Fix images (totally didn't forget to commit last time)

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -17,31 +17,54 @@ const Carousel = () => {
 
   return (
     <div
-      className="row-start-1 rounded-2xl md:col-start-2 md:col-end-4 row-span-2 overflow-hidden"
+      className="row-start-1 rounded-2xl md:col-start-2 md:col-end-4 md:row-span-2 overflow-hidden"
       ref={emblaRef}
     >
       <div className="flex">
-        <div className="relative h-80 lg:h-[50vh]  max-h-[450px] min-w-0 grow-0 shrink-0 basis-full">
-          <Image alt="Slider Text" src="/placeimg_720_720_any.jpeg" fill />
+        <div className="relative h-full min-w-0 grow-0 shrink-0 basis-full">
+          <Image alt="Slider Text" src="/000000.png"   
+          width={760}
+          height={400}
+        />
           <div className="p-4 bottom-0 absolute text-xl bg-raisin bg-opacity-75 z-99 w-full h-16">
-            <span className="text-ivory font-bold tracking-wider">Hello</span>
+            <span className="text-ivory font-bold tracking-wider">Hello1</span>
           </div>
         </div>
-        <div className="relative h-80 lg:h-[50vh]  max-h-[450px] min-w-0 grow-0 shrink-0 basis-full">
-          <Image alt="Slider Text" src="/placeimg_720_720_any.jpeg" fill />
+        <div className="relative h-full min-w-0 grow-0 shrink-0 basis-full">
+          <Image alt="Slider Text" src="/000000.png"   
+          width={760}
+          height={400}
+        />
           <div className="p-4 bottom-0 absolute text-xl bg-raisin bg-opacity-75 z-99 w-full h-16">
-            <span className="text-ivory font-bold tracking-wider">Hello</span>
+            <span className="text-ivory font-bold tracking-wider">Hello1</span>
           </div>
         </div>
-        <div className="relative h-80 lg:h-[50vh]  max-h-[450px] min-w-0 grow-0 shrink-0 basis-full">
-          <Image alt="Slider Text" src="/placeimg_720_720_any.jpeg" fill />
+        <div className="relative h-full min-w-0 grow-0 shrink-0 basis-full">
+          <Image alt="Slider Text" src="/000000.png"   
+          width={760}
+          height={400}
+        />
           <div className="p-4 bottom-0 absolute text-xl bg-raisin bg-opacity-75 z-99 w-full h-16">
-            <span className="text-ivory font-bold tracking-wider">Hello</span>
+            <span className="text-ivory font-bold tracking-wider">Hello1</span>
           </div>
-        </div>{" "}
+        </div>
       </div>
     </div>
   );
 };
+/*
 
+        <div className="relative h-full md:h-80 lg:h-[50vh]  max-h-[450px] min-w-0 grow-0 shrink-0 basis-full">
+          <Image alt="Slider Text" src="/placeimg_720_720_any.jpeg" fill />
+          <div className="p-4 bottom-0 absolute text-xl bg-raisin bg-opacity-75 z-99 w-full h-16">
+            <span className="text-ivory font-bold tracking-wider">Hello2</span>
+          </div>
+        </div>
+        <div className="relative h-full md:h-80 lg:h-[50vh]  max-h-[450px] min-w-0 grow-0 shrink-0 basis-full">
+          <Image alt="Slider Text" src="/placeimg_720_720_any.jpeg" fill />
+          <div className="p-4 bottom-0 absolute text-xl bg-raisin bg-opacity-75 z-99 w-full h-16">
+            <span className="text-ivory font-bold tracking-wider">Hello3</span>
+          </div>
+        </div>
+*/
 export default Carousel;
