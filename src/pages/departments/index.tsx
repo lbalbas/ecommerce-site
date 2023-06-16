7abe5d6575f438c8bc72ec58196536a7bd292bc5
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-06-16 14:57:25 -0400

    Add/fix dpts page

--- src/pages/departments/index.tsx
+++ src/pages/departments/index.tsx
@@ -30,12 +30,12 @@ const Departments = () => {
       <h3 className="tracking-widest text-lg uppercase font-bold text-raisin my-4">
         Departments
       </h3>
-      <div className="grid w-full grid-cols-3 gap-6">
+      <div className="grid w-full grid-cols-2 md:grid-cols-3 gap-6">
         {depts.map((dept) => {
           return (
             <Link
               className="relative w-full h-full flex rounded-2xl justify-center items-center"
-              href={"/departments/" + dept.name}
+              href={"/departments/" + dept.id}
             >
               <Image
                 alt="Departments"
@@ -45,7 +45,7 @@ const Departments = () => {
                 src={dept.image}
               />
               <div className="absolute rounded-2xl bg-raisin opacity-50 w-full h-full flex items-center justify-center"></div>
-              <span className="absolute text-ivory uppercase font-bold">
+              <span className="text-center absolute text-ivory uppercase font-bold">
                 {dept.name}
               </span>
             </Link>
