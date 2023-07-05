7f85870e1616c8d3d6eed273bac925f1636da585
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-07-05 13:37:09 -0400

    Add missing api version option

--- src/pages/api/checkout_sessions/index.ts
+++ src/pages/api/checkout_sessions/index.ts
@@ -2,28 +2,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: req.body.transfItems,
-        mode: "payment",
-        success_url: `${req.headers.origin}/?checkoutSuccess=true`,
-        cancel_url: `${req.headers.origin}/?checkoutSuccess=false`,
-      });
+	if (req.method === "POST") {
+		if (!process.env.STRIPE_SECRET_KEY) {
+			console.error("Stripe public key is not defined in env variables");
+			return;
+		}
+		const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+			apiVersion: "2022-11-15",
+		});
+		try {
+			const session = await stripe.checkout.sessions.create({
+				payment_method_types: ["card"],
+				line_items: req.body.transfItems,
+				mode: "payment",
+				success_url: `${req.headers.origin}/?checkoutSuccess=true`,
+				cancel_url: `${req.headers.origin}/?checkoutSuccess=false`,
+			});
 
-      res.status(200).json({ id: session.id });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  } else {
-    res.setHeader("Allow", "POST");
-    res.status(405).end("Method Not Allowed");
-  }
+			res.status(200).json({ id: session.id });
+		} catch (error: any) {
+			res.status(500).json({ error: error.message });
+		}
+	} else {
+		res.setHeader("Allow", "POST");
+		res.status(405).end("Method Not Allowed");
+	}
 }
