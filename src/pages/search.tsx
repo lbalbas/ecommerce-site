8832fbcf95dc6202ead2ad937281ef178d66be9f
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2023-07-08 13:47:23 -0400

    Add message when no results

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,53 +3,62 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
 import LoadingBlock from "../components/Loading";
+import Head from "next/head";
 
 const Search = () => {
-	const [loading, setLoading] = useState(true);
-	const router = useRouter();
-	const search = router.query.s;
-	let stringSearch = "";
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const search = router.query.s;
+  let stringSearch = "";
 
-	if (Array.isArray(search)) {
-		// If Search is an array, take the first element
-		stringSearch = search[0];
-	} else if (search) {
-		// If Search is a string
-		stringSearch = search;
-	}
+  if (Array.isArray(search)) {
+    // If Search is an array, take the first element
+    stringSearch = search[0];
+  } else if (search) {
+    // If Search is a string
+    stringSearch = search;
+  }
 
-	const searchItems = trpc.search.useQuery(
-		{ search: stringSearch },
-		{
-			enabled: stringSearch !== "",
-		}
-	);
+  const searchItems = trpc.search.useQuery(
+    { search: stringSearch },
+    {
+      enabled: stringSearch !== "",
+    }
+  );
 
-	const items = searchItems.data;
+  const items = searchItems.data;
 
-	if (searchItems.isLoading) {
-		return 				<LoadingBlock size={10} />;
-;
-	} else if (items) {
-		return (
-			<div className="flex min-h-[60vh] flex-col">
-				<h3 className="font-bold tracking-wider uppercase my-4">
-					{`Searching "${search}"`}
-				</h3>
-				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-					{items.map((item) => {
-						return (
-							<ProductItem
-								list={false}
-								key={item.id}
-								data={item}
-							/>
-						);
-					})}
-				</div>
-			</div>
-		);
-	}
+  if (searchItems.isLoading) {
+    return (
+      <div>
+        {" "}
+        <Head>
+          <title>{`Search Page`}</title>
+        </Head>
+        <LoadingBlock size={10} />
+      </div>
+    );
+  } else if (items) {
+    return (
+      <div className="flex min-h-[60vh] flex-col">
+        <Head>
+          <title>{`Searching for "${search}"`}</title>
+        </Head>
+        <h3 className="font-bold tracking-wider uppercase my-4">
+          {`Searching "${search}"`}
+        </h3>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {items.length === 0 ? (
+            <p>No matching results found</p>
+          ) : (
+            items.map((item) => (
+              <ProductItem list={false} key={item.id} data={item} />
+            ))
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Search;
